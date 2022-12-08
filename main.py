16cdf08b4cf38f9573ee04a296929b5bf323ae07
Yuliia Osiv <yosiv@kse.org.ua>
2022-12-08 18:31:46 +0200

    renamed file

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+file_with_data = sys.argv[1]
+country = sys.argv[3]
+year = sys.argv[]
+print(len(sys.argv[1:]))
+
+
+print("country = ", country)
+if country == "USA":
+    print("Oops")
+
